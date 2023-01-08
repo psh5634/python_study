9a3907b5c85a512e6dee69932be9fe56482ecc02
SooHyun Park <psh5634@naver.com>
2023-01-08 13:06:57 +0900

    1.0

---
+++ 10.py
@@ -0,0 +1,14 @@
+# 인덱스와 슬라이싱
+
+lang = 'PYTHON'
+
+print(lang[0])  # 처음 시작이 1이 아니라 0부터 시작 = print(lang[-6])  # 첫 번째 값
+print(lang[5])
+print(lang[-1])  # 인덱스  # 마지막 값 
+print(lang[1:6])  
+print(lang[1:])
+print(lang[:4])
+print(lang[:])  # 슬라이싱 
+
+#몇 번째 = 인덱스 
+#어디부터 : 어디까지 = 슬라이싱 
