03f1a7f6273b10707a828891d494bd34d35b8dea
SooHyun Park <psh5634@naver.com>
2023-01-09 19:33:38 +0900

    1.0

---
+++ 11.py
@@ -0,0 +1,24 @@
+# 문자열 처리 
+
+snack = '꿀꽈배기'
+two = '2개'
+juseyo = snack + two  # 꿀꽈배기 2개
+
+juseyo = juseyo + '주세요'
+juseyo += '주세요'  # 꿀꽈배기 2개 주세요 
+
+num = 3
+num += 2
+num -= 1
+num *= 2
+num /= 4
+
+snack = '꿀꽈배기'
+print(len(snack))  # 길이(length)
+
+snack = '''꿀꽈배기는
+너무
+맛있어요'''  # 여러 줄 문자
+
+
+
