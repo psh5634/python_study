9d4ab3449b32e75f7ef913b5d70bafa5172b4f45
SooHyun Park <psh5634@naver.com>
2023-01-10 09:39:32 +0900

    1.0

---
+++ 12.py
@@ -0,0 +1,12 @@
+# 문자열 메소드 1
+
+letter = 'how are YOU?'
+
+print(letter.lower())  # 모든 내용을 소문자로 
+print(letter.upper())  # 모든 내용을 대문자로 
+print(letter.capitalize())  # 첫 글자만 대문자로 
+print(letter.title())  # 각 단어들의 첫 글자만 대문자로 
+print(letter.swapcase())  # 대소문자를 서로 바꾸기 
+print(letter.split())  # 문자열을 나누기 
+print(letter.count('how'))  # 'how'가 몇 번 쓰였는지 
+
